ca444a7a27ce4a5eeeac57d14e5546f2634396b9
Steve von Takach <steve@advancedcontrol.com.au>
2012-02-21 12:44:50 +1100

    Fixed routes and manifest

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 	
 	scope "/interfaces" do
-		resources :flexible do
+		resources :flexible_interface do
 			get	:manifest,	:on => :member
 			get	:alt,	:on => :member
 		end
